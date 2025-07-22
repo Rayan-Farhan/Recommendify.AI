5e8ca353bce7b8cda0a11d19f6c07d261dbb1225
Rayan Farhan <rayanfarhan1902@gmail.com>
2025-07-23 01:24:12 +0500

    Added streamlit frontend

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import requests
+import json
+
+st.set_page_config()
+st.title("Recommendify.AI")
+
+st.markdown("""
+This is an **AI-powered Product Review Analyzer** that finds YouTube reviews and helps you decide whether to buy a product, with the help of AI Agents & LLM!
+
+No more wasting time browsing countless YouTube videos to find trustworthy product reviews!
+
+How it works:
+- Searches YouTube for top review videos
+- Filter relevant videos
+- Extracts transcripts of the videos  
+- Summarizes the overall sentiment by using the pros and cons from transcripts
+- Provides a recommendation based on the summaries
+
+Just enter a specific product name below to begin!
+""")
+
+query = st.text_input("Enter a specific product name")
+st.caption("Examples: Sony PS5, iPhone 15 Pro, Dyson Airwrap, Tesla Model 3 2024, HyperX Cloud 2")
+
+if st.button("Recommendify!"):
+    if not query.strip():
+        st.warning("Please enter a product name")
+    else:
+        st.write("⏳ Running analysis...")
+        try:
+            response = requests.post(
+                "http://localhost:8000/analyze",
+                json={"query": query}
+            )
+
+            if response.status_code != 200:
+                st.error(f"Server error: {response.status_code}")
+                st.stop()
+
+            data = response.json()
+
+            # Case: Generic product
+            if not data.get("success"):
+                st.warning(f"{data.get('error', 'Unknown error')}")
+                st.stop()
+
+            # Case: Successful analysis
+            st.success("✅ Analysis complete!")
+
+            st.subheader("Final Verdict")
+
+            try:
+                #
+                verdict_data = json.loads(data["verdict"])
+                recommendation = verdict_data.get("recommendation", "N/A")
+                reason = verdict_data.get("reason", "No reason provided.")
+
+                st.markdown(f"**Recommendation:** {recommendation}")
+                st.markdown(f"**Reason:** {reason}")
+
+            except Exception:
+                st.write(data["verdict"])
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
